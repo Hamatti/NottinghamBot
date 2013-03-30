1df63f73390e5a27edc8189c63ad927fcaea978e
Hamatti <jumasan@utu.fi>
2013-03-30 14:52:32 +0100

    Created custom exceptions

---
+++ bot_exceptions.py
@@ -0,0 +1,23 @@
+class TitleException(Exception):
+	pass
+
+class PoemException(Exception):
+	pass
+
+class WikiException(Exception):
+	pass
+
+class RestaurantException(Exception):
+	pass
+
+class SteamException(Exception):
+	pass
+
+class DecisionException(Exception):
+	pass
+
+class TodoException(Exception):
+	pass
+
+class HelpException(Exception):
+	pass
